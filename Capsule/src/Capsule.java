02df8caef8fe0554f0eb14ada034f493b9ffa5ab
mitsuaki0304 <mitsuaki0304@gmail.com>
2018-11-14 21:43:22 +0900

    kadai

---
+++ Capsule/src/Capsule.java
@@ -0,0 +1,10 @@
+
+public class Capsule {
+
+	public static void main(String[] args) {
+		Person taro = new Person("山田太郎",30);
+		System.out.println(taro.getName());
+		System.out.println(taro.getAge());
+	}
+
+}
---
+++ Person2/src/Test1.java
@@ -0,0 +1,32 @@
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+		Person taro = new Person();
+		taro.name="taro";
+		taro.age=18;
+		System.out.println(taro.name);
+		System.out.println(taro.age);
+
+		Person jiro = new Person("taro",20);
+		System.out.println(jiro.name);
+		System.out.println(jiro.age);
+
+		Person saburo = new Person("saburo");
+		System.out.println(saburo.name);
+		System.out.println(saburo.age);
+
+		Person namaenasi = new Person(25);
+		System.out.println(namaenasi.name);
+		System.out.println(namaenasi.age);
+
+		Person hanako = new Person("hanako",17);
+		System.out.println(hanako.name);
+		System.out.println(hanako.age);
+
+
+	}
+
+}
