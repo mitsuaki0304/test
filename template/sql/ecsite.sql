e886236915952d15dc80639eb2e6b9f3e4a1e041
ikeyoshi <yoshpond0147@gmail.com>
2018-11-28 21:40:17 +0900

    181128

--- template/sql/ecsite.sql
+++ template/sql/ecsite.sql
@@ -1,6 +1,6 @@
 set names utf8;
 set foreign_key_checks=0;
-drop databases if exists ecsite;
+drop database if exists ecsite;
 
 create database if not exists ecsite;
 use ecsite;
@@ -43,7 +43,7 @@ create table item_info_transaction(
  INSERT INTO item_info_transaction(item_name, item_price, item_stock)
  VALUES("ノートBook",100,50);
  INSERT INTO login_user_transaction(login_id,login_pass,user_name)
- VALUES("internous","internous01,"test");
+ VALUES("internous","internous01","test");
 
 
 
--- template/src/com/internousdev/template/util/DBConnector.java
+++ template/src/com/internousdev/template/util/DBConnector.java
@@ -7,9 +7,11 @@ import java.sql.SQLException;
 public class DBConnector {
 
 	private static String driverName = "com.mysql.jdbc.Driver";
-	private static String url = "jdbc:mysql://localhost/testdb";
 
-	static String user = "root";
+	private static String url = "jdbc:mysql://localhost/ecsite";
+
+	private static String user = "root";
+
 	private static String password = "mysql";
 
 	public Connection getConnection() {
